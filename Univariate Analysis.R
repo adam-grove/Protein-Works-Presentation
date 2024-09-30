f876e28e0c09adcb26dde06c3a356b5911b44829
Adam Grove <152027749+adam-grove@users.noreply.github.com>
2024-09-30 16:15:41 +0100

    Add files via upload

---
+++ Univariate Analysis.R
@@ -0,0 +1,83 @@
+---
+title: "Univariate Analysis"
+output: html_document
+date: "2024-09-30"
+---
+```{r}
+library(moments)
+```
+```{r}
+Sale_Price <- read.csv("Sales_data.csv",header=T)
+
+```
+
+# Summary Statistics
+```{R}
+summary(Sale_Price)
+
+
+```
+
+```{R}
+
+# Calculate and print summary statistics for unit_sale_price_gbp
+mean_unit_sale_price_gbp <- mean(Sale_Price$unit_sale_price_gbp)
+median_unit_sale_price_gbp <- median(Sale_Price$unit_sale_price_gbp)
+sd_unit_sale_price_gbp <- sd(Sale_Price$unit_sale_price_gbp)
+var_unit_sale_price_gbp <- var(Sale_Price$unit_sale_price_gbp)
+skewness_unit_sale_price_gbp <- skewness(Sale_Price$unit_sale_price_gbp)
+kurtosis_unit_sale_price_gbp <- kurtosis(Sale_Price$unit_sale_price_gbp)
+
+# Print the results
+print(paste0("Mean: ", mean_unit_sale_price_gbp))
+print(paste0("Median: ", median_unit_sale_price_gbp))
+print(paste0("Standard Deviation: ", sd_unit_sale_price_gbp))
+print(paste0("Variance: ", var_unit_sale_price_gbp))
+print(paste0("Skewness: ", skewness_unit_sale_price_gbp))
+print(paste0("Kurtosis: ", kurtosis_unit_sale_price_gbp))
+```
+
+# Analysis of results
+Adressing the skewness of the data. The mean of the data is larger than the median, as wel las a skewness value of 1.38 suggesting that the data is *right skewed*
+
+```{R}
+hist(Sale_Price$unit_sale_price_gbp,main = "Histogram of Sale Price" ,xlab = "Sales Price", ylab = "Frequency", col = "lightblue")
+```
+This histogram has a long tail towards the right (right skew). This can be stastically represented by Kurtosis of 5.21 which indicates the heavy tail.
+
+```{R}
+plot(density(Sale_Price$unit_sale_price_gbp), main = "Density Plot of Sales Prices", xlim = c(0,70))
+
+```
+# Testing for Normality
+The normality of data can affect the choice of stastical tests and models created. The shape of the data suggests that it does not follow a normal distribution but there are additional tests that we can use. The example I've used here is a *Shapiro-Wilk* test
+```{R}
+shapiro.test(Sale_Price$unit_sale_price_gbp)
+
+```
+This test gave a very small p value. Meaning we can say with confidence that the data is not normally distributed. This can be visualised in a Q-Q plot. The closer the points are to the normal line, the more likely the data follows a normal distribution 
+```{R}
+qqnorm(Sale_Price$unit_sale_price_gbp)
+qqline(Sale_Price$unit_sale_price_gbp)
+```
+In this diagram, the data deviated from the line in the 2nd and 3rd quartiles, again suggesting it does not follow a normal distribution. It also helps us to confirm the right skew of the data 
+
+# Investigating outliers
+We know from the shape of the histogram and the Kurtosis value that there are some large values that are skewing the data. These can be visualised by a boxplot 
+```{R}
+boxplot(Sale_Price$unit_sale_price_gbp,main = "Box Plot of Sales Prices", col = "lightblue")
+
+```
+This boxplot shows that there are values that are outside limits to be considered an outlier and and will skew the data.
+
+# Conclusions 
+This dataset has a wide variance, numbers are spread far out and there are extremes at the larger end that are causing right skew
+
+The mean of this data will not be ideal to use as there are distruptions to the *central tendancy of the data*. In this case it will be most useful to use the **median** instead of the mean. As it is less affected by the skew of the data and will be more representative of an average unit price
+
+## Some considerations
+With larger datasets median may not be so effective 
+1. Computational power used to calculate the median is higher
+2. Medians are less useful with lots of zero values (median number of pregnancies per copulation is zero)
+3. There are some data types where median is as useful. E.g. In sales, removing the tails might underestimate your numbers
+
